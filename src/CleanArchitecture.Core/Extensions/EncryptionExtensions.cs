9511a60520b9dd2c251b8011281afa44c951e30b
FLAVIO BARBOSA NETO <flavio.barbosa@verity.com.br>
2022-05-15 22:10:49 -0300

    Encapsulamento de regras de negócios para o usuário, juntamente com a encriptografia para senha do usuário e metódo de criptografia como extensão.

---
+++ src/CleanArchitecture.Core/Extensions/EncryptionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CleanArchitecture.Core.Extensions
+{
+    public class EncryptionExtensions
+    {
+        public static string EncryptString(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using MemoryStream memoryStream = new();
+                using CryptoStream cryptoStream = new((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+                using (StreamWriter streamWriter = new(cryptoStream))
+                {
+                    streamWriter.Write(plainText);
+                }
+
+                array = memoryStream.ToArray();
+            }
+
+            return Convert.ToBase64String(array);
+        }
+    }
+}
